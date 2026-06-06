8e841b3362c34bc684b1c945bf36a87a1847be8e
anshdogar <anshdogar@gmail.com>
2026-06-06 18:21:47 +0200

    Added padding, fixed issues with shifting and realloc
    Added padding to messages so that bytes do not unexpectedly get lost when trying to make full length. Sets values AFTER storing local data on the stack so that they do not get lost in the process.

--- customallocator.c
+++ customallocator.c
@@ -2,9 +2,11 @@
 
 #include <unistd.h> 
 
-typedef struct block{
+struct block{
 
-    char size;
+    int size;
+
+    int pad;
 
     void*start;
 
@@ -12,7 +14,7 @@ typedef struct block{
 
     struct block* next;
 
-} ;
+};
 
 //basically linked list structure
 
@@ -33,12 +35,14 @@ struct block* custommalloc(char bytes){
 
     printf("creating space for %d bytes\n", bytes);
 
-    void* fullPointer = sbrk(bytes + sizeof(struct block
+    int pad = (8 - (bytes % 8) % 8);
+
+    void* fullPointer = sbrk(bytes + pad + sizeof(struct block
     )); //update current position, add space for full
 
     currentPosition = fullPointer;
 
-    struct block* b = (struct block*)((char*)currentPosition + bytes);
+    struct block* b = (struct block*)((char*)currentPosition + bytes + 1);
 
     printf("current position = %p\n", currentPosition);
 
@@ -53,7 +57,7 @@ struct block* custommalloc(char bytes){
     }
 
 
-    b->size = bytes + sizeof(struct block);
+    b->size = bytes + sizeof(struct block) + pad;
 
     
     printf("Block contains %d bytes\n", b->size);
@@ -61,6 +65,8 @@ struct block* custommalloc(char bytes){
     b->prev = currentBlock;
 
     b->next = 0;
+
+    b->pad = pad;
 
     currentBlock = b;
 
@@ -76,14 +82,43 @@ struct block* shiftBlock(struct block* block, int bytes){
         printf("Block contains %d bytes\n", block->size);
 
         printf("Shifting block %d bytes\n", bytes);
+
+        printf("Block starts at %p\n", block->start);
+
+        printf("Block ends at %p\n", ((char*)block->start + block->size));
+
+        int size = block->size;
+
+        int pad = block->pad;
+
+        struct block* next = block->next;
+
+        struct block* prev = block->prev;
+
+        void* start = block->start;
     
-    for(void*  i = block->start + block->size; i > block->start; i--){
+    for(char*  i = (char*)start + 1; i <= (char*)(start + size); i++){
+
         char* j = i + bytes;
+        // printf("changing pointer location %p\n", i);
 
         (*j) = *(char*)i;
+
+
+        // printf("going to pointer location %p\n", j);
+
+        printf("value at location %p is %d\n", j, *j);
     }
 
-    block->start = (void*)((char*)block->start - bytes);
+    block->start = (void*)((char*)block->start + bytes);
+
+    block->size = size;
+
+    block->prev = prev;
+
+    block->next = next;
+
+    block->pad = pad;
 
     printf("New block start is at %p\n", block->start);
 
@@ -107,18 +142,24 @@ void customfree(struct block* b){
 
     struct block* a = b->next;
 
-    while (a != 0)
-    {
-       shiftBlock(a, -(b->size));
-       a = a->next;
-    }
-
-    currentPosition -= b->size;
+    int size = b->size;
 
     b->size = 0;
     b->prev = 0;
     b->next = 0;
     b->start = 0;
+    b->pad = 0;
+
+    while (a != 0)
+    {
+        struct block* next = a->next;
+       shiftBlock(a, -(size));
+       a = next;
+    }
+
+    currentPosition = (void*)((char*)currentPosition - size);
+
+    printf("Current position is %p\n", currentPosition);
 }
 
 
@@ -130,11 +171,24 @@ struct block* customrealloc(struct block* b, char newBytes){
         return 0;
     }
 
+    int oldbytes = b->size - sizeof(struct block) - b->pad;
+
+    printf("The bytes of the old structure is %d\n", oldbytes);
+
     struct block* newBlock = custommalloc(newBytes);
 
-    for(char i = newBytes; i > 0; i--){
-        *((char*) b->start + newBytes) = *((char*) newBlock->start + newBytes);
+    int i = 0;
+
+    while(i < oldbytes && i < newBytes){
+
+        printf("Character to copy is %d\n",*((char*)b->start + i + 1) );
+        *((char*)newBlock->start + i + 1) = *((char*)b->start + i + 1);
+
+        printf("Character at %p is %d\n", newBlock->start + i + 1, *((char*)newBlock->start + i + 1) );
+        i++;
     }
+    
+
     customfree(b);
 
     return newBlock;
@@ -147,7 +201,22 @@ int main(){
 
     struct block* second = custommalloc(7);
 
+    char*s = (char*)second->start;
+
+    printf("start address is %p\n", s);
+
+    int x = 1;
+
+    for(char*i = s + 1; i < s + second->size - sizeof(struct block); i++){
+        *i = x;
+        x++;
+
+        printf("Value at location %p is %d\n", i, *i);
+    }
+
     customfree(first);
 
     struct block* third = custommalloc(9);
+
+    second = customrealloc(second, 10);
 }
